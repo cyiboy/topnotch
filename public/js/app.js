7eb1cbc315fd5fe06cdef3490ebcf0008a1d49a6
victor <vagbenro@gmail.com>
2020-10-06 11:31:03 +0100

    latest push

--- public/js/app.js
+++ public/js/app.js
@@ -20,5 +20,5 @@ var firebaseConfig = {
 
 // index page 
 // contest reqigation
-//var mountainsRef = storageRef.child('assets/img/twitter.png');
+//var mountainsRef = storageRef.child('..//assets/img/twitter.png');
        
